b82bb59125f7e47547a51f6b9280137648f59194
yasmin <yasmin.tiomkin@gmail.com>
2020-06-18 00:07:25 +0300

    avoid obstacles

---
+++ Assets/Filter Scripts/PhysicsLayerFilter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Filter/Physics Layer")]
+public class PhysicsLayerFilter : ContextFilter
+{
+    public LayerMask mask;
+
+    public override List<Transform> Filter(FlockAgent agent, List<Transform> original)
+    {
+        List<Transform> filtered = new List<Transform>();
+        foreach (Transform item in original)
+        {
+            // check if object is in the same layer
+            if (mask == (mask | (1 << item.gameObject.layer)))
+            {
+                // our item is on the same layer as our mask is check against
+                // the obstacles are in a different layer so they will be filtered out
+                filtered.Add(item); 
+            }
+        }
+
+        return filtered;
+    }
+}
